5f7b8e32060f233198410f912188a84a4d6aa814
Alexandra Oshmyanskaya <aoshmyanskaya@gmail.com>
2016-08-07 18:53:00 +0200

    Add new table (project), new method - create_project, new test, refactoring, token parsing

---
+++ backend/ddl/user.sql
@@ -0,0 +1,29 @@
+CREATE SEQUENCE user_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 14
+  CACHE 1;
+ALTER TABLE user_id_seq
+  OWNER TO postgres;
+
+CREATE TABLE public."user"
+(
+   id integer NOT NULL DEFAULT nextval('user_id_seq'), 
+   login character varying(255), 
+   password character varying(255), 
+   name character varying(255), 
+   company character varying(255), 
+   email character varying(255), 
+   is_confirmed boolean DEFAULT false,
+   last_login_date timestamp without time zone, 
+   CONSTRAINT user_pkey PRIMARY KEY (id),
+  
+   CONSTRAINT user_login_key UNIQUE (login)
+) 
+WITH (
+  OIDS = FALSE
+)
+;
+ALTER TABLE public."user"
+  OWNER TO postgres;
--- backend/server.js
+++ backend/server.js
@@ -35,129 +35,138 @@ function query(text, values, cb) {
 	});
 }
 
+function successFalseCb(msg, callback) {
+	var result = {
+        	'success': false,
+                'msg': '' + msg
+        };
+        if (callback != null) {
+        	callback(null, result);
+        }
+}
+
+function mergeJson(obj1, obj2) {
+	var result={};
+	for(var key in obj1) result[key]=obj1[key];
+	for(var key in obj2) result[key]=obj2[key];
+	return result;
+}
+
+function successCb(callback, additionalParams) {
+	var result = {
+		'success': true
+	}
+	if (additionalParams) { 
+		result = mergeJson(result, additionalParams);
+	}
+        if (callback != null) {
+                callback(null, result);
+        }
+}
+
 function createUser(login, password, name, company, email, callback) {
 	try {
+		console.log('call method createUser: login = ' + login + ', name = ' + name + ', company = ' + company +', email = ' + email);
 		isLoginExists(login, function(err, result) {
 			if (err) {
-				var result = {
-                                	'success': false,
-					'msg': err
-                                };
-				if (callback != null) {
-                                        callback(null, result);
-                                }
+				successFalseCb(err, callback);
 				return;
 			}
 			var isLoginEx = result;
 			if (isLoginEx) {
-                                var result = {
-                                        'success': false,
-                                        'msg': 'Login already exists'
-                                };
-                                if (callback != null) {
-                                        callback(null, result);
-                                }
-                                return;
-                        } else {
-				query('INSERT INTO public.user(login, password, name, company, email)' +
-	                                ' VALUES ($1, $2, $3, $4, $5);', [login, password, name, company, email], function(err, result) {
-					var result = {
-		                                'success': true
-                       			};
-					if (err) {
-						result.success = false;
-						result.msg = err;
-					}
-		                        if (callback != null) {
-                       			        callback(null, result);
-		                        }
-				});
+				successFalseCb('Login ' + login + '  already exists', callback);
+				return;
 			}
+			query('INSERT INTO public.user(login, password, name, company, email)' +
+	                	' VALUES ($1, $2, $3, $4, $5);', [login, password, name, company, email], function(err, result) {
+				if (err) {
+					successFalseCb(err, callback);
+				} else {
+					successCb(callback);
+				}
+			});
 
 		});
 	} catch (err) {
-                var result = {
-			'success': false,
-                        'msg': err
-                };
-		if (callback != null) {
-			callback(null, result);
-		}
+		console.log('error in method createUser: ' + err);
+		successFalseCb(err, callback);
 	}
 }
 
+function createProject(userId, projectName, callback) {
+        try {
+		console.log('call method createProject: userId = ' + userId + ', projectName = ' + projectName);
+                isProjectExists(userId, projectName, function(err, result) {
+                        if (err) {
+                                successFalseCb(err, callback);
+                                return;
+                        }
+                        var isProjectEx = result;
+                        if (isProjectEx) {
+                                successFalseCb('Project ' + projectName + ' already exists', callback);
+                                return;
+                        }
+                        query('INSERT INTO public.project(user_id, project_name)' +
+                                ' VALUES ($1, $2) RETURNING id;', [userId, projectName], function(err, result) {
+                                if (err) {
+                                        successFalseCb(err, callback);
+                                } else {
+					var row = result.rows[0];
+					var newProjectId = row.id;
+					successCb(callback, {
+						'new_project_id': newProjectId
+					});
+                                }
+                        });
+
+                });
+        } catch (err) {
+                console.log('error in method createProject: ' + err);
+                successFalseCb(err, callback);
+        }
+}
+
+
 function getToken(user) {
-	var token = jwt.sign(user, config.tokenKey);
-	return token;
+	return jwt.sign(user, config.tokenKey);
+}
+
+function checkToken(token, callback) {
+        jwt.verify(token, config.tokenKey, callback);
 }
 
 function checkAuth(login, password, callback) {
 	console.log('call method checkAuth');
 	try {
 		isLoginExists(login, function(err, result) {
-                        if (err) {
-                                var result = {
-                                        'success': false,
-                                        'msg': err
-                                };
-                                if (callback != null) {
-                                        callback(null, result);
-                                }
-                                return;
+			if (err) {
+				successFalseCb(err, callback);
+				return;
                         }
 			var isLoginEx = result;
                         if (!isLoginEx) {
-                                var result = {
-                                        'success': false,
-                                        'msg': 'Login ' + login + ' doesn\'t exist'
-                                };
-                                if (callback != null) {
-                                        callback(null, result);
-                                }
+				successFalseCb('Login ' + login + ' doesn\'t exist', callback);
                                 return;
                         } else {
 				getUserInfo(login, function(err, user) {
 					if (err) {
-		                                var result = {
-        	                        	        'success': false,
-                	        	                'msg': err
-                        		        };
-                	                	if (callback != null) {
-		                                       	callback(null, result);
-	        	                        }
+						successFalseCb(err, callback);
                 	                	return;
                         		}
 					//console.log(JSON.stringify(user));
 					if (user.password == password) {
-						var result = {
-                        	        	        'success': true,
-                	        	                'msg': null,
+						successCb(callback, {
 							'token': getToken(user)
-		                                };
-				                if (callback != null) {
-                        		                callback(null, result);
-			                        }
+						});
 					} else {
-						 var result = {
-                                                        'success': false,
-                                                        'msg': 'incorrect password for user ' + login
-                                                };
-                                                if (callback != null) {
-                                                        callback(null, result);
-                                                }
+						successFalseCb('incorrect password for user ' + login, callback);
 					}
 				});
 			}
 		});
 	} catch (err) {
 		console.log('error in method checkAuth: ' + err);
-                var result = {
-                        'success': false,
-                        'msg': err
-                };
-                if (callback != null) {
-                        callback(null, result);
-                }
+		successFalseCb(err, callback);
 	}
 }
 
@@ -166,13 +175,7 @@ function getUserInfo(login, callback) {
 	try {
 		query('SELECT * from public.user where login = $1', [login], function(err, result) {
                         if (err) {
-                                var result = {
-                                        'success': false,
-                                        'msg': err
-                                };
-                                if (callback != null) {
-                                        callback(null, result);
-                                }
+				successFalseCb(err, callback);
                                 return;
                         }
 			var user = result.rows[0];
@@ -183,28 +186,16 @@ function getUserInfo(login, callback) {
 		});
 	} catch (err) {
 		console.log('error in method getUserInfo: ' + err);
-		var result = {
-                        'success': false,
-                        'msg': err
-                };
-                if (callback != null) {
-                        callback(null, result);
-                }
+		successFalseCb(err, callback);
 	}
 }
 
 function isLoginExists(login, callback) {
-	console.log('call method isLoginExists');
+	console.log('call method isLoginExists, login = ' + login);
 	try {
                	query('SELECT count(*) from public.user where login = $1', [login], function(err, result) {
 			if (err) {
-                       	        var result = {
-               	                        'success': false,
-       	                                'msg': err
-                                };
-                               	if (callback != null) {
-                       	                callback(null, result);
-               	                }
+				successFalseCb(err, callback);
        	                        return;
                         }
 
@@ -219,18 +210,49 @@ function isLoginExists(login, callback) {
 		});
 	} catch (err) {
 	        console.log('error in method isLoginExists: ' + err);
-		var result = {
-                        'success': false,
-                        'msg': err
-                };
-                if (callback != null) {
-                        callback(null, result);
-                }
-		console.log('error: ' + err);
+		successFalseCb(err, callback);
 	}
 }
 
+function isProjectExists(userId, projectName, callback) {
+        console.log('call method isProjectExists, userId = ' + userId + ', projectName = ' + projectName);
+	
+        try {
+                query('SELECT count(*) from public.project where user_id = $1 and project_name = $2', [userId, projectName], function(err, result) {
+                        if (err) {
+				successFalseCb(err, callback);
+				return;
+			}
+                        //console.log(count);
+
+                        var count = result.rows[0];
+                        var projectExists = count.count != 0;
+                        console.log('result of method isProjectExists: ' + projectExists);
+                        if (callback != null) {
+                                callback(null, projectExists);
+                        }
+                });
+        } catch (err) {
+                console.log('error in method isProjectExists: ' + err);
+		successFalseCb(err, callback);
+        }
+}
+
 var io1 = require('socket.io').listen(3040);
+
+function checkIfNotEmptyMessage(socket, message, methodName, cb) {
+	if (message == null) {
+		socket.emit(methodName, {
+                	'success': false,
+                        'msg': 'message is empty'
+                });
+		return;
+	}
+	//if message is not empty - proceed
+	if (cb != null) {
+		cb();
+	}
+}
 
 io1.on('connection', function(socket1) {
 	console.log("client connected");
@@ -238,41 +260,56 @@ io1.on('connection', function(socket1) {
 	//signup method
 	socket1.on('signup', function(message)      {
 	        console.log('received singup message: ' + JSON.stringify(message));
-		if (message == null) {
-			socket1.emit('signup_response', {
-				'success': false,
-				'msg': 'message is empty'
-			});
-			return;
-		}
-		var login = message.login;
-		var password = message.password;
-		var name = message.name;
-		var company = message.company;
-		var email = message.email;
-		createUser(login, password, name, company, email, function(err, result) {
-			console.log('send singup result: ' + JSON.stringify(result));
-			socket1.emit('signup_response', result);
+		checkIfNotEmptyMessage(socket1, message, 'signup_response', function() {
+			var login = message.login;
+        	        var password = message.password;
+	                var name = message.name;
+                	var company = message.company;
+        	        var email = message.email;
+	                createUser(login, password, name, company, email, function(err, result) {
+                	        console.log('send singup result: ' + JSON.stringify(result));
+        	                socket1.emit('signup_response', result);
+	                });
 		});
 	});
 
 	//authenticate method
         socket1.on('authenticate', function(message) {
 		console.log('received authenticate message: ' + JSON.stringify(message));
-                if (message == null) {
-                        socket1.emit('authenticate_response', {
-                                'success': false,
-                                'msg': 'message is empty'
-                        });
-                        return;
-                }
-		var login = message.login;
-                var password = message.password;
+		checkIfNotEmptyMessage(socket1, message, 'authenticate_response', function() {
+			var login = message.login;
+	                var password = message.password;
 				
-		checkAuth(login, password, function(err, result) {
-			console.log('send authenticate result: ' + JSON.stringify(result));
-			socket1.emit('authenticate_response', result);
+			checkAuth(login, password, function(err, result) {
+				console.log('send authenticate result: ' + JSON.stringify(result));
+				socket1.emit('authenticate_response', result);
+			});
 		});
 	});
+
+	//create_project
+        socket1.on('create_project', function(message) {
+                console.log('received create_project message: ' + JSON.stringify(message));
+		checkIfNotEmptyMessage(socket1, message, 'authenticate_response', function() {
+	                var projectName = message.project_name;
+        	        var token = message.token;
+			checkToken(token, function(err, result) {
+				if (err) {
+					socket1.emit('create_project_response', {
+	                        	        'success': false,
+        	        	                'msg': 'check token error: ' + err
+        		                });
+	                	        return;
+				}
+				var userInfo = result;
+				console.log('Token parse result: ' + JSON.stringify(result));
+				
+				createProject(userInfo.id, projectName, function(err, result) {
+                        	        console.log('send create project response: ' + JSON.stringify(result))
+                                	socket1.emit('create_project_response', result);
+				});
+			});
+		});
+        });
 });
 
