f1cf0c3273407cd9c42b2c021016b76b60304558
BigDaddy <bigdaddy@BigDaddys-Mac.local>
2016-08-17 03:05:35 -0400

    first trial with droplet

--- app/js/module.js
+++ app/js/module.js
@@ -1,3 +1,3 @@
 angular.module('Blurbiz.socket', ['btford.socket-io']);
-angular.module('Blurbiz.project', ['ui.bootstrap']);
+angular.module('Blurbiz.project', ['ui.bootstrap', 'ngDroplet']);
 angular.module('Blurbiz', ['ui.bootstrap', 'ui.router', 'ngCookies', 'Blurbiz.socket', 'Blurbiz.project']);
