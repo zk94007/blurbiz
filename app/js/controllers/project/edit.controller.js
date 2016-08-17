7d37190f4a0c7857a5180b4869ab6f43751fff25
BigDaddy <bigdaddy@BigDaddys-Mac.local>
2016-08-17 13:51:49 -0400

    trial to use Upload.upload for file upload, seems to not work with socket.io

--- app/js/controllers/project/edit.controller.js
+++ app/js/controllers/project/edit.controller.js
@@ -5,47 +5,43 @@
         .module('Blurbiz.project')
         .controller('Project.EditController', Controller);
 
-    function Controller($state, $stateParams, $scope, $uibModal, socket, ProjectService, FlashService, LocalStorageService) {
+    function Controller($state, $stateParams, $scope, $uibModal, $timeout, socket, Upload, ProjectService, FlashService, LocalStorageService) {
 
         var token = LocalStorageService.getToken();
 
         $scope.interface = {};
 
-        // Listen for when the interface has been configured.
-        $scope.$on('$dropletReady', function whenDropletReady() {
+        $scope.uploadFiles = function (files) {
+            if (files && files.length) {
+                for (var i = 0; i < files.length; i++) {
+                  var file = files[i];
+                  if (!file.$error) {
+                    Upload.upload({
+                        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                        data: {
+                          file: file  
+                        }
+                    }).then(function (resp) {
+                        $timeout(function() {
+                            $scope.log = 'file: ' +
+                            resp.config.data.file.name +
+                            ', Response: ' + JSON.stringify(resp.data) +
+                            '\n' + $scope.log;
 
-            $scope.interface.allowedExtensions(['png', 'jpg', 'bmp', 'gif', 'svg', 'torrent']);
-            $scope.interface.setRequestUrl('upload.html');
-            $scope.interface.defineHTTPSuccess([/2.{2}/]);
-            $scope.interface.useArray(false);
+                            $scope.result = response.data;
+                        });
+                    }, null, function (evt) {
+                        var progressPercentage = parseInt(100.0 *
+                                evt.loaded / evt.total);
+                        $scope.log = 'progress: ' + progressPercentage + 
+                            '% ' + evt.config.data.file.name + '\n' + 
+                          $scope.log;
+                    });
+                  }
+                }
+            }
+        }
 
-        });
-
-        // Listen for when the files have been successfully uploaded.
-        $scope.$on('$dropletSuccess', function onDropletSuccess(event, response, files) {
-
-            $scope.uploadCount = files.length;
-            $scope.success     = true;
-            console.log(response, files);
-
-            $timeout(function timeout() {
-                $scope.success = false;
-            }, 5000);
-
-        });
-
-        // Listen for when the files have failed to upload.
-        $scope.$on('$dropletError', function onDropletError(event, response) {
-
-            $scope.error = true;
-            console.log(response);
-
-            $timeout(function timeout() {
-                $scope.error = false;
-            }, 5000);
-
-        });
-        
         initController();
 
         function initController() {
